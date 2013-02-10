d64692350386375d5884eae1967092b4368e0f33
Aditya Raisinghani <aditya.ajeet@gmail.com>
2013-02-11 01:43:27 +0530

    Installed simple_form and updated front end for forms
    Also added sign in, sign out, register links on navbar
    Updated forms to bootstrap
    
    Getting a hang of ruby! :P

--- app/models/status.rb
+++ app/models/status.rb
@@ -1,3 +1,11 @@
 class Status < ActiveRecord::Base
-  attr_accessible :content, :name
+  attr_accessible :content, :user_id
+  belongs_to :user #making associations between users
+  #and their statues and their ids
+  #we write belong to user and ruby understands
+  #that there is something called user_id
+  #convention over configuration
 end
+
+#lists the fields that can be modified in database by
+#using a form
