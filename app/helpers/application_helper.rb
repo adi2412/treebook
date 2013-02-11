502c13acdcd985037a387b6b6381ba14f0ebf346
Aditya Raisinghani <aditya.ajeet@gmail.com>
2013-02-11 12:48:32 +0530

    Styled flash messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
-module ApplicationHelper
+module ApplicationHelper #add helper classes here
+
+	def flash_class(type) #this class helps differentiate between alerts and errors in the application.html.erb
+		case type
+		when :alert
+			"alert-error"
+		when :notice
+			"alert-success"
+		else
+			""
+		end
+	end
 end
